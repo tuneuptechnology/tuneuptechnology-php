3bd1b895304c27ef8ee63d0ba0886084c7f3c51d
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-09-13 16:44:05 -0600

    fix: update the cassettes directory permissions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ use VCR\VCR;
 use allejo\VCR\VCRCleaner;
 
 if (!file_exists('tests/cassettes')) {
-    mkdir('tests/cassettes', 0777, true);
+    mkdir('tests/cassettes', 0755, true);
 }
 
 VCR::configure()->setCassettePath('tests/cassettes')
